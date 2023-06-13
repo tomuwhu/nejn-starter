bee9e699bbb5d60e208782149f69fd36c6cf2f51
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-06-13 07:48:33 +0200

    .

--- static/engine.js
+++ static/engine.js
@@ -5,7 +5,12 @@ $(() => {
     $("body")
         .append(`
             <br>
-            <h1>NEJN (NeDB - ExpressJS - JQueryUI - NodeJS) Starter</h1>
+            <h1>NEJN Starter</h1>
+            <h5>(<a href="https://www.npmjs.com/package/nedb?activeTab=readme" target="_blank"
+            >NeDB</a> - <a href="https://expressjs.com/en/starter/hello-world.html" target="_blank"
+            >ExpressJS</a> - <a href="https://jqueryui.com/" target="_blank"
+            >JQueryUI - <a href="https://nodejs.org/en/about" target="_blank"
+            >NodeJS)</h5>
         `)
         .append(`<div class="lev">${
             Levenshtein("cica","picsa")
